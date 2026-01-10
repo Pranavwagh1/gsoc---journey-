dcb57ba92c654c00d1c73aa30aee1d3c04fbe4cf
Pranavwagh1 <waghpranav11@gmail.com>
2026-01-10 15:43:22 +0530

    categorize age according to given input age

--- day1.py
+++ day1.py
@@ -1,13 +1,13 @@
-def Check_even_odd(user_input):
+def Category_acording_to_your_age(age):
+    if age < 13:
+        return "Child"
+    elif age >=13 and age <= 19:
+        return "Teenager"
+    elif age >= 20 and age <= 59 :
+        return "Adult"
+    else : 
+        return "Senior Citizen"
+    
+age = int(input("Enter your age = "))
 
-     if(user_input%2 == 0):
-       return "Even Number"
-     else:
-        return "Odd Number"
-
-
-
-user_input = int(input("Enter a numbe to check its even or odd = ")) 
-
-
-print(Check_even_odd(user_input))
+print("Your category according to your age is",Category_acording_to_your_age(age))
