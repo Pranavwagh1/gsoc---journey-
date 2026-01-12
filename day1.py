5f2da56b84d92b9f7202ad7101d9ea33f07e3bb0
Pranavwagh1 <waghpranav11@gmail.com>
2026-01-12 19:44:37 +0530

    made a dictionary to store students name and their marks
    Replaced list of marks with a dictionary of student marks and updated the print logic to display student names and their marks.

--- day1.py
+++ day1.py
@@ -1,11 +1,7 @@
-marks = [55,95,99,12,43,23,89,23,90]
-new_marks = []
-
-def is_valid_distinction(mark):
-      return mark>=75 and mark<=100
-
-for mark in marks:
-    if is_valid_distinction(mark):
-        new_marks.append(mark)
-
-print(new_marks)
+student_marks = {
+    "Pranav" : 90,
+    "Amit" : 80,
+    "Isha" : 87
+}
+for name , mark in student_marks.items():
+    print(name,mark)
