c4d51ee36e0e304dbe306e6f52d06e31089b0b6f
Pranavwagh1 <waghpranav11@gmail.com>
2026-01-12 14:12:46 +0530

    Implement pass counting for students based on marks
    Added functionality to count and display passed students based on their marks.

--- day2.py
+++ day2.py
@@ -1,6 +1,14 @@
-students = ["Pranav", "Rahul", "Amit"]
+marks = [55,95,99,12,43,23,89,23,90]
+passed = 0 ;
 
-students.append("Bro")
+def is_pass(mark):
+    if mark>=40:
+        return True 
+    else:
+        return False
+    
+for mark in marks:
+    if is_pass(mark):
+       passed+=1 
 
-for student in students:
-    print(student)
+print("Passed students =",passed)
