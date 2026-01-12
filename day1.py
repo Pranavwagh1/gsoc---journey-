dfe506b2fcda6053eff02d1737c592169c683b16
Pranavwagh1 <waghpranav11@gmail.com>
2026-01-12 19:54:34 +0530

    Looped through a distionary using a function and created a new distonary for those who satisfy the condition

--- day1.py
+++ day1.py
@@ -1,7 +1,15 @@
 student_marks = {
     "Pranav" : 90,
-    "Amit" : 80,
+    "Amit" : 40,
     "Isha" : 87
 }
+passed_students = {}
+
+def is_passed(mark):
+    return mark >= 75
+
 for name , mark in student_marks.items():
-    print(name,mark)
+   if is_passed(mark):
+    passed_students[name]=mark
+
+print(passed_students)
