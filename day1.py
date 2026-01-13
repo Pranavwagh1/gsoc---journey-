79732d3d9e82ad500604ef9be51d477da474aa37
Pranavwagh1 <waghpranav11@gmail.com>
2026-01-13 18:07:11 +0530

    mini project that checks if student is passed or failed , if the studenyt is passed we creadte a new list of passed student and add the passed student to it , and same for failed students also we create a new list of failed student and add failed student to it and in last we print these pass and failed students name and also printed the lengh of pass and failed student list

--- day1.py
+++ day1.py
@@ -1,12 +1,30 @@
+def is_passed(student):
+    return student["marks"] >= 75
+
+
 students = [
     {"name": "Pranav", "marks": 90},
     {"name": "Amit", "marks": 40},
-    {"name": "Isha", "marks": 87}
+    {"name": "Isha", "marks": 87},
+    {"name": "Rahul", "marks": 65}
 ]
+
 passed_students = []
+failed_students = []
 
 for student in students:
-    if(student["marks"]>=75):
-       passed_students.append(student)
+    if is_passed(student):
+        passed_students.append(student)
+    else:
+        failed_students.append(student)
 
-print(passed_students)
+print("Passed students:")
+for student in passed_students:
+    print(student["name"])
+
+print("\nFailed students:")
+for student in failed_students:
+    print(student["name"])
+
+print("\nTotal Passed:", len(passed_students))
+print("Total Failed:", len(failed_students))
